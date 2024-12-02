caae9f411a3ad175620ffd1a9a38f042b285ecc0
Ruslan Zalata <rz@fabmicro.ru>
2024-12-02 20:38:08 +0000

    Add lab 1_basics/1_00_fulladder1: a 1-bit fulladder example with KEY inputs and LED outputs.

---
+++ labs/1_basics/1_00_fulladder1/fulladder1.sv
@@ -0,0 +1,10 @@
+module fulladder1 (
+        input  wire       c_in,
+        input  wire       a,
+        input  wire       b,
+        output wire       sum,
+        output wire       c_out,
+);
+        assign c_out = (a & b) | (c_in & (a ^ b));
+	assign sum = (c_in ^ (a ^ b));
+endmodule
